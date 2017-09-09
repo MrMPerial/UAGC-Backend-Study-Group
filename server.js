0b124e33489014bc8f2a8895ee6f53f7bac4869f
Christine Skorenko <cskorenko@hotmail.com>
2017-09-08 20:10:18 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/', routes);
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000');
+});
+
+module.exports = app;
